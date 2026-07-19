49e997b0c21f3295c3d6773a1f76fd64343f644f
KSamtoclef <samtoclef@gmail.com>
2026-07-19 01:51:24 -0700

    Stage 5: integrate store.js with server chat

--- assets/js/core/store.js
+++ assets/js/core/store.js
@@ -1,4 +1,4 @@
-const state = { session: null, user: null, home: null, route: '/', loading: true };
+const state = { session: null, user: null, home: null, wallet: null, activeChat: null, route: '/', loading: true };
 const listeners = new Set();
 
 export const store = {
