f6e8a1a4fc0d44720044227b88c722aa5a5aa45f
KSamtoclef <samtoclef@gmail.com>
2026-07-19 01:02:25 -0700

    Add assets/js/core/config.js

---
+++ assets/js/core/config.js
@@ -0,0 +1,6 @@
+export const config = Object.freeze({
+  supabaseUrl: 'https://ijjmgrfgyqtsvigzjdjp.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlqam1ncmZneXF0c3ZpZ3pqZGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0NjMxMzcsImV4cCI6MjA5OTAzOTEzN30.TFOOIJDK9YXa8mSBXcgQ2IqIfqD-0UhcBhIEN8QxeDE',
+  currency: 'NGN',
+  locale: 'en-NG'
+});
